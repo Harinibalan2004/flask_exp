526d33cc2a53299f9f6203b279ab34224e114c51
your-name <your-email-address>
2024-06-08 10:11:27 +0530

    Add first and second page with navigation

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return "Flask Working fine"
+@app.route("/pageone")
+def pageone():
+    return render_template("firstpage.html")
+@app.route("/pagetwo")
+def pagetwo():
+    return render_template("secondpage.html")
 if __name__ == "__main__": 
-    app.run()
-@app.route("/login")
-def template():
-    return render_template("index.html")
+    app.run(debug=True)
