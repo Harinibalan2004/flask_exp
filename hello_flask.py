e2c954980b8014e74dc1a7f10ac90d80d47aca08
your-name <your-email-address>
2024-06-08 11:12:34 +0530

    Add frontpage, resultpage, and routing logic

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import determine
 app = Flask(__name__)
-@app.route("/pageone")
-def pageone():
-    return render_template("firstpage.html")
-@app.route("/pagetwo")
-def pagetwo():
-    return render_template("secondpage.html")
+@app.route("/frontpage")
+def frontpage  
+    return render_template("frontpage.html")
+@app.route("/resultpage")
+def resultpage():
+    return render_template("resultpage.html")
+@app.route("/printtime")
+def printtime():
+    current_time = datetime.datetime.now()
+    print(f"current DateTime: {current_time}")
+    return redirect("/resultpage")
 if __name__ == "__main__": 
     app.run(debug=True)
